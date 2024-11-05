0c111b60769dc7dbd587282ad3db815075b401f5
timmPrice <timmPrice6@gmail.com>
2024-11-05 00:01:04 -0500

    fixing several minor things. untracking temp files

--- aoc-machine/obj/Debug/net8.0/aoc-machine.AssemblyInfo.cs
+++ aoc-machine/obj/Debug/net8.0/aoc-machine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aoc-machine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ce428bf3a6b6fec1c70b2a00fbe21d62749e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261723e49aa7e56ba64a85cc54956ed96b0e76a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("aoc-machine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aoc-machine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
