dcb654d6eae138fad8d11253e45e9d4107453b2a
timmPrice <timmPrice6@gmail.com>
2024-11-05 00:29:40 -0500

    should be a minimum viable product

--- aoc-machine/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-@if [ -f "./puzzle.txt" ]; then rm ./puzzle.txt; fi
-@if [ -f "./puzzle-info.txt" ];then rm ./puzzle-info.txt; fi
-@if [ -f "./puzzle-input.txt" ];then rm ./puzzle-input.txt; fi
