3a08403c511e53d302d926a7ad4d36c34119d6d7
timmPrice <timmPrice6@gmail.com>
2024-11-05 13:39:38 -0500

    day1 c-sharp part 1

---
+++ 2023/c-sharp/day1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace aoc_2022;
+
+public class Program{
+
+    public static void Main(string[] args){
+        int answer = 0;
+        
+        using(StreamReader read = File.OpenText("puzzle-input.txt")){
+            string input;
+            while((input = read.ReadLine()) != null){
+                answer += FindDigits(input);
+            }
+        }
+        Console.WriteLine(answer);
+    }
+
+    public static int FindDigits(string input){
+            int value = 0; string number = "";
+            
+            for(int i = 0; i < input.Length; i++){
+                if(char.IsDigit(input[i])){
+                    number += input[i];
+                }
+            } 
+           
+            if(number.Length >= 2){
+                value = Convert.ToInt32(number[0].ToString() + number[number.Length - 1].ToString());
+            } else {
+                value = Convert.ToInt32(number + number);
+            }
+            
+            return value;
+    }
+
+    public static int SumDigist(int fd){
+        int sum = 0;
+        
+        return sum;
+
+    }
+}
+
